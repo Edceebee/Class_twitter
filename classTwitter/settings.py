54412043feb3800974854774c00c12db954155e9
Chinonso Edozie <edceebee@gmail.com>
2021-05-20 09:25:58 +0100

    added files

--- classTwitter/settings.py
+++ classTwitter/settings.py
@@ -127,6 +127,10 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
+STATIC_ROOT = Path.joinpath(BASE_DIR, 'staticfiles')
+
+STATIC_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
